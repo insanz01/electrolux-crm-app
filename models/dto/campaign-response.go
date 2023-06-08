43a325b74216700ae391db649baafd5907d3c97b
insanz01 <insankamil002@gmail.com>
2023-06-08 10:46:47 +0700

    [FIX] change data type of purchase date

--- models/dto/campaign-response.go
+++ models/dto/campaign-response.go
@@ -1,23 +1,23 @@
 package dto
 
-import "github.com/lib/pq"
+import "time"
 
 type Campaign struct {
-	Id               string      `db:"id"`
-	Name             string      `db:"name"`
-	ChannelAccountId string      `db:"channel_account_id"`
-	ClientId         string      `db:"client_id"`
-	City             []string    `db:"city"`
-	CountRepeat      *int        `db:"count_repeat"`
-	IsRepeated       bool        `db:"is_repeated"`
-	IsScheduled      bool        `db:"is_scheduled"`
-	ModelType        []string    `db:"model_type"`
-	ProductLine      []string    `db:"product_line"`
-	PurchaseDate     pq.NullTime `db:"purchase_date"`
-	ScheduleDate     pq.NullTime `db:"schedule_date"`
-	ServiceType      []string    `db:"service_type"`
-	Status           string      `db:"status"`
-	TemplateId       string      `db:"template_id"`
+	Id               string     `db:"id"`
+	Name             string     `db:"name"`
+	ChannelAccountId string     `db:"channel_account_id"`
+	ClientId         string     `db:"client_id"`
+	City             []string   `db:"city"`
+	CountRepeat      *int       `db:"count_repeat"`
+	IsRepeated       bool       `db:"is_repeated"`
+	IsScheduled      bool       `db:"is_scheduled"`
+	ModelType        []string   `db:"model_type"`
+	ProductLine      []string   `db:"product_line"`
+	PurchaseDate     *time.Time `db:"purchase_date"`
+	ScheduleDate     *time.Time `db:"schedule_date"`
+	ServiceType      []string   `db:"service_type"`
+	Status           string     `db:"status"`
+	TemplateId       string     `db:"template_id"`
 }
 
 type CampaignsResponse struct {
