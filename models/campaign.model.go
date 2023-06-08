4499dcc32e50845743d26f68f5062f37ecc3b5a8
insanz01 <insankamil002@gmail.com>
2023-06-08 08:43:32 +0700

    change type of datetime

--- models/campaign.model.go
+++ models/campaign.model.go
@@ -2,26 +2,25 @@ package models
 
 import (
 	"time"
-
-	"github.com/lib/pq"
 )
 
 type Campaign struct {
-	Id               string      `db:"id"`
-	Name             string      `db:"name"`
-	ChannelAccountId string      `db:"channel_account_id"`
-	ClientId         string      `db:"client_id"`
-	City             []string    `db:"city"`
-	CountRepeat      *int        `db:"count_repeat"`
-	IsRepeated       bool        `db:"is_repeated"`
-	IsScheduled      bool        `db:"is_scheduled"`
-	ModelType        []string    `db:"model_type"`
-	ProductLine      []string    `db:"product_line"`
-	PurchaseDate     pq.NullTime `db:"purchase_date"`
-	ScheduleDate     pq.NullTime `db:"schedule_date"`
-	ServiceType      []string    `db:"service_type"`
-	Status           string      `db:"status"`
-	TemplateId       string      `db:"template_id"`
+	Id               string     `db:"id"`
+	Name             string     `db:"name"`
+	ChannelAccountId string     `db:"channel_account_id"`
+	ClientId         string     `db:"client_id"`
+	City             []string   `db:"city"`
+	CountRepeat      *int       `db:"count_repeat"`
+	NumOfOccurence   *int       `db:"num_of_occurence"`
+	IsRepeated       bool       `db:"is_repeated"`
+	IsScheduled      bool       `db:"is_scheduled"`
+	ModelType        []string   `db:"model_type"`
+	ProductLine      []string   `db:"product_line"`
+	PurchaseDate     *time.Time `db:"purchase_date"`
+	ScheduleDate     *time.Time `db:"schedule_date"`
+	ServiceType      []string   `db:"service_type"`
+	Status           string     `db:"status"`
+	TemplateId       string     `db:"template_id"`
 }
 
 type CampaignHistory struct {
--- models/dto/campaign-request.go
+++ models/dto/campaign-request.go
@@ -1,20 +1,23 @@
 package dto
 
-import "github.com/lib/pq"
+import (
+	"time"
+)
 
 type CampaignInsertRequest struct {
-	Name             string      `json:"name"`
-	ChannelAccountId string      `json:"channel_account_id"`
-	ClientId         string      `json:"client_id"`
-	City             []string    `json:"city"`
-	CountRepeat      *int        `json:"count_repeat"`
-	IsRepeated       bool        `json:"is_repeated"`
-	IsScheduled      bool        `json:"is_scheduled"`
-	ModelType        []string    `json:"model_type"`
-	ProductLine      []string    `json:"product_line"`
-	PurchaseDate     pq.NullTime `json:"purchase_date"`
-	ScheduleDate     pq.NullTime `json:"schedule_date"`
-	ServiceType      []string    `json:"service_type"`
-	Status           string      `json:"status"`
-	TemplateId       string      `json:"template_id"`
+	Name             string     `json:"name"`
+	ChannelAccountId string     `json:"channel_account_id"`
+	ClientId         string     `json:"client_id"`
+	City             []string   `json:"city"`
+	CountRepeat      *int       `json:"count_repeat"`
+	NumOfOccurence   *int       `json:"num_of_occurence"`
+	IsRepeated       bool       `json:"is_repeated"`
+	IsScheduled      bool       `json:"is_scheduled"`
+	ModelType        []string   `json:"model_type"`
+	ProductLine      []string   `json:"product_line"`
+	PurchaseDate     *time.Time `json:"purchase_date"`
+	ScheduleDate     *time.Time `json:"schedule_date"`
+	ServiceType      []string   `json:"service_type"`
+	Status           string     `json:"status"`
+	TemplateId       string     `json:"template_id"`
 }
