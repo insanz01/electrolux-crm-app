0a74d7d87ff00226de6faca1d88b5415bb972989
insanz01 <insankamil002@gmail.com>
2023-06-22 09:03:40 +0700

    fix issue in get single reports

--- repository/report.repository.go
+++ repository/report.repository.go
@@ -15,7 +15,7 @@ type ReportRepository interface {
 
 const (
 	getAllReportsQuery    = "SELECT campaign.id, campaign.name as campaign_name, channel_account.id as channel_account_id, channel_account.name as channel_name, client.id as client_id, client.name as division, campaign.created_at, campaign.status FROM campaign JOIN channel_account ON campaign.channel_account_id = channel_account.id JOIN client ON campaign.client_id = client.id"
-	getSingleReportsQuery = "SELECT campaign.id, campaign.name as campaign_name, channel.name as channel_name, channel_account.id as channel_account_id, channel_account.name as channel_account_name, client.id as client_id, client.name as created_by, '' as division, '' as message_id, '' as contact, campaign.template_id, '' as template_name, '' as template_category, '' as template_language, '' as content_type, '' as approved_at, '' as approved_by, '' as wa_id, '' as reply_button, '' as reply_at, campaign.status as state, '' as invalid, campaign_customer.sent_at, campaign_customer.delivered_at, campaign_customer.read_at, campaign_summary.failed_sent as failed_at, '' as failed_detail, campaign.created_at, campaign_customer.customer_id FROM campaign JOIN channel_account ON campaign.channel_account_id = channel_account.id JOIN client ON campaign.client_id = client.id JOIN campaign_summary ON campaign.id = campaign_summary.campaign_id JOIN campaign_customer ON campaign_summary.id = campaign_customer.summary_id WHERE deleted_at is null AND campaign.id = $1"
+	getSingleReportsQuery = "SELECT campaign.id, campaign.name as campaign_name, channel.name as channel_name, channel_account.id as channel_account_id, channel_account.name as channel_account_name, client.id as client_id, client.name as created_by, '' as division, '' as message_id, '' as contact, campaign.template_id, '' as template_name, '' as template_category, '' as template_language, '' as content_type, '' as approved_at, '' as approved_by, '' as wa_id, '' as reply_button, '' as reply_at, campaign.status as state, '' as invalid, campaign_customer.sent_at, campaign_customer.delivered_at, campaign_customer.read_at, campaign_summary.failed_sent as failed_at, '' as failed_detail, campaign.created_at, campaign_customer.customer_id FROM campaign JOIN channel_account ON campaign.channel_account_id = channel_account.id JOIN channel ON channel_account.channel_id = channel.id JOIN client ON campaign.client_id = client.id JOIN campaign_summary ON campaign.id = campaign_summary.campaign_id JOIN campaign_customer ON campaign_summary.id = campaign_customer.summary_id WHERE campaign.deleted_at is null AND campaign.id = $1"
 )
 
 func (r *Repository) GetAllReports() ([]*models.Report, error) {
@@ -67,7 +67,7 @@ func (r *Repository) GetAllReportsByFilter(filter dto.ReportProperties) ([]*mode
 	return reports, nil
 }
 
-func (r *Repository) GetSingleReport(id string) (*models.DownloadReport, error) {
+func (r *Repository) GetSingleReport(id string) ([]*models.DownloadReport, error) {
 	var reports []*models.DownloadReport
 
 	err := r.db.Select(&reports, getSingleReportsQuery, id)
@@ -75,5 +75,5 @@ func (r *Repository) GetSingleReport(id string) (*models.DownloadReport, error) 
 		return nil, err
 	}
 
-	return reports[0], nil
+	return reports, nil
 }
--- services/report.service.go
+++ services/report.service.go
@@ -79,30 +79,35 @@ func (rs *reportService) Request(c echo.Context, request dto.ReportDownloadReque
 }
 
 func (rs *reportService) Download(c echo.Context, id string) (*dto.ReportDownloadResponse, error) {
-	report, err := rs.repository.GetSingleReport(id)
+	reports, err := rs.repository.GetSingleReport(id)
 	if err != nil {
 		return nil, err
 	}
 
-	customerDetail, _ := rs.repository.GetSingle(report.CustomerId)
+	theReports := []*models.DownloadReport{}
+	for _, report := range reports {
+		customerDetail, _ := rs.repository.GetSingle(report.CustomerId)
 
-	custMobileNo := ""
-	state := "true"
+		custMobileNo := ""
+		state := "true"
 
-	for _, c := range customerDetail {
-		if c.Key == "mobile_no" {
-			custMobileNo = c.Value
+		for _, c := range customerDetail {
+			if c.Key == "mobile_no" {
+				custMobileNo = c.Value
+			}
 		}
+
+		if custMobileNo != "" {
+			report.Contact = custMobileNo
+			state = "false"
+		}
+
+		report.Invalid = state
+
+		theReports = append(theReports, report)
 	}
 
-	if custMobileNo != "" {
-		report.Contact = custMobileNo
-		state = "false"
-	}
-
-	report.Invalid = state
-
-	filename, err := rs.generateFile(id, report)
+	filename, err := rs.generateFile(id, theReports)
 	if err != nil {
 		return nil, err
 	}
@@ -120,7 +125,7 @@ func (rs *reportService) Download(c echo.Context, id string) (*dto.ReportDownloa
 	}, nil
 }
 
-func (rs *reportService) generateFile(id string, report *models.DownloadReport) (string, error) {
+func (rs *reportService) generateFile(id string, reports []*models.DownloadReport) (string, error) {
 	fileName := fmt.Sprintf("uploads/%s.xlsx", id)
 
 	file := excelize.NewFile()
@@ -152,32 +157,36 @@ func (rs *reportService) generateFile(id string, report *models.DownloadReport) 
 	file.SetCellValue("Sheet1", "Y1", "FailedAt")
 	file.SetCellValue("Sheet1", "Z1", "FailedDetail")
 
-	file.SetCellValue("Sheet1", "A2", report.CreatedBy)
-	file.SetCellValue("Sheet1", "B2", report.DivisionName)
-	file.SetCellValue("Sheet1", "C2", report.BroadcastId)
-	file.SetCellValue("Sheet1", "D2", report.ChannelAccountName)
-	file.SetCellValue("Sheet1", "E2", report.CampaignName)
-	file.SetCellValue("Sheet1", "F2", report.MessageId)
-	file.SetCellValue("Sheet1", "G2", report.ChannelName)
-	file.SetCellValue("Sheet1", "H2", report.Contact)
-	file.SetCellValue("Sheet1", "I2", report.TemplateName)
-	file.SetCellValue("Sheet1", "J2", report.TemplateCategory)
-	file.SetCellValue("Sheet1", "K2", report.TemplateLanguage)
-	file.SetCellValue("Sheet1", "L2", report.ContentType)
-	file.SetCellValue("Sheet1", "M2", report.CreatedDate)
-	file.SetCellValue("Sheet1", "N2", report.CreatedBy)
-	file.SetCellValue("Sheet1", "O2", report.ApprovedAt)
-	file.SetCellValue("Sheet1", "P2", report.ApprovedBy)
-	file.SetCellValue("Sheet1", "Q2", report.WAID)
-	file.SetCellValue("Sheet1", "R2", report.ReplyButton)
-	file.SetCellValue("Sheet1", "S2", report.ReplyAt)
-	file.SetCellValue("Sheet1", "T2", report.State)
-	file.SetCellValue("Sheet1", "U2", report.Invalid)
-	file.SetCellValue("Sheet1", "V2", report.SentAt)
-	file.SetCellValue("Sheet1", "W2", report.DeliveredAt)
-	file.SetCellValue("Sheet1", "X2", report.ReadAt)
-	file.SetCellValue("Sheet1", "Y2", report.FailedAt)
-	file.SetCellValue("Sheet1", "Z2", report.FailedDetail)
+	for idx, report := range reports {
+		cellNumber := idx + 1
+
+		file.SetCellValue("Sheet1", fmt.Sprintf("A%d", cellNumber), report.CreatedBy)
+		file.SetCellValue("Sheet1", fmt.Sprintf("B%d", cellNumber), report.DivisionName)
+		file.SetCellValue("Sheet1", fmt.Sprintf("C%d", cellNumber), report.BroadcastId)
+		file.SetCellValue("Sheet1", fmt.Sprintf("D%d", cellNumber), report.ChannelAccountName)
+		file.SetCellValue("Sheet1", fmt.Sprintf("E%d", cellNumber), report.CampaignName)
+		file.SetCellValue("Sheet1", fmt.Sprintf("F%d", cellNumber), report.MessageId)
+		file.SetCellValue("Sheet1", fmt.Sprintf("G%d", cellNumber), report.ChannelName)
+		file.SetCellValue("Sheet1", fmt.Sprintf("H%d", cellNumber), report.Contact)
+		file.SetCellValue("Sheet1", fmt.Sprintf("I%d", cellNumber), report.TemplateName)
+		file.SetCellValue("Sheet1", fmt.Sprintf("J%d", cellNumber), report.TemplateCategory)
+		file.SetCellValue("Sheet1", fmt.Sprintf("K%d", cellNumber), report.TemplateLanguage)
+		file.SetCellValue("Sheet1", fmt.Sprintf("L%d", cellNumber), report.ContentType)
+		file.SetCellValue("Sheet1", fmt.Sprintf("M%d", cellNumber), report.CreatedDate)
+		file.SetCellValue("Sheet1", fmt.Sprintf("N%d", cellNumber), report.CreatedBy)
+		file.SetCellValue("Sheet1", fmt.Sprintf("O%d", cellNumber), report.ApprovedAt)
+		file.SetCellValue("Sheet1", fmt.Sprintf("P%d", cellNumber), report.ApprovedBy)
+		file.SetCellValue("Sheet1", fmt.Sprintf("Q%d", cellNumber), report.WAID)
+		file.SetCellValue("Sheet1", fmt.Sprintf("R%d", cellNumber), report.ReplyButton)
+		file.SetCellValue("Sheet1", fmt.Sprintf("S%d", cellNumber), report.ReplyAt)
+		file.SetCellValue("Sheet1", fmt.Sprintf("T%d", cellNumber), report.State)
+		file.SetCellValue("Sheet1", fmt.Sprintf("U%d", cellNumber), report.Invalid)
+		file.SetCellValue("Sheet1", fmt.Sprintf("V%d", cellNumber), report.SentAt)
+		file.SetCellValue("Sheet1", fmt.Sprintf("W%d", cellNumber), report.DeliveredAt)
+		file.SetCellValue("Sheet1", fmt.Sprintf("X%d", cellNumber), report.ReadAt)
+		file.SetCellValue("Sheet1", fmt.Sprintf("Y%d", cellNumber), report.FailedAt)
+		file.SetCellValue("Sheet1", fmt.Sprintf("Z%d", cellNumber), report.FailedDetail)
+	}
 
 	if err := file.SaveAs(fileName); err != nil {
 		return "", err
